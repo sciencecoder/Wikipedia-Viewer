360ac5ed4543fec9d2b59b0a7499be1ca353d5db
Adam Orr <adamorr234@gmail.com>
2016-05-18 07:32:07 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+$("button").on("click", function() {
+   var input = $("#input").val(); 
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + input + "&limit=10&callback=?";
+   $.getJSON(url, function(data) {
+       $(".searchResult").html("");
+ for(var i = 0; i < data[2].length; i++) {
+       $(".searchResult").append("<div class='room col-md-12'>" + data[2][i] + "</div>");
+       }
+  
+      
+ 
+      
+     
+   })
+   
+   
+   
+  
+  
+})
